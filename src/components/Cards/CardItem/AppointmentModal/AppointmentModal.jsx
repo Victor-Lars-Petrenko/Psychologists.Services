82dba40292f0511752a1ef1a92635254694e474d
Victor Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2024-07-03 23:57:22 +0300

    add appointment modal styles

--- src/components/Cards/CardItem/AppointmentModal/AppointmentModal.jsx
+++ src/components/Cards/CardItem/AppointmentModal/AppointmentModal.jsx
@@ -4,51 +4,62 @@ import Button from "components/Button";
 import css from "./AppointmentModal.module.css";
 
 const AppointmentModal = ({ close, name, avatar }) => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    close();
+  };
+
   return (
     <Modal close={close}>
-      <h2 className={css.modalTitle}>
-        Make an appointment with a psychologists
-      </h2>
-      <p className={css.modalDescription}>
-        You are on the verge of changing your life for the better. Fill out the
-        short form below to book your personal appointment with a professional
-        psychologist. We guarantee confidentiality and respect for your privacy.
-      </p>
-      <div className={css.psychologistWrap}>
-        <img src={avatar} alt={name} className={css.avatar} />
-        <div className={css.textWrap}>
-          <p className={css.explanation}>Your psychologists</p>
-          <h3 className={css.name}>{name}</h3>
+      <form onSubmit={handleSubmit} className={css.appointmentModal}>
+        <h2 className={css.modalTitle}>
+          Make an appointment with a psychologists
+        </h2>
+        <p className={css.modalDescription}>
+          You are on the verge of changing your life for the better. Fill out
+          the short form below to book your personal appointment with a
+          professional psychologist. We guarantee confidentiality and respect
+          for your privacy.
+        </p>
+        <div className={css.psychologistWrap}>
+          <img src={avatar} alt={name} className={css.avatar} />
+          <div className={css.textWrap}>
+            <p className={css.explanation}>Your psychologists</p>
+            <h3 className={css.name}>{name}</h3>
+          </div>
         </div>
-      </div>
-      <input
-        type="text"
-        required
-        className={css.modalInput}
-        placeholder="Name"
-      />
-      <div>
         <input
-          type="tel"
+          type="text"
           required
           className={css.modalInput}
-          placeholder="+380"
+          placeholder="Name"
         />
+        <div className={css.inputWrap}>
+          <input
+            type="tel"
+            required
+            className={css.modalInput + " " + css.wrappedInput}
+            placeholder="+380"
+          />
+          <input
+            type="time"
+            required
+            className={css.modalInput + " " + css.wrappedInput}
+            placeholder="00:00"
+          />
+        </div>
         <input
-          type="time"
+          type="email"
           required
           className={css.modalInput}
-          placeholder="00:00"
+          placeholder="Email"
         />
-      </div>
-      <input
-        type="email"
-        required
-        className={css.modalInput}
-        placeholder="Email"
-      />
-      <textarea className={css.modalTextarea} placeholder="Comment" />
-      <Button text="Send" width="100%" height="52px" type="submit" />
+        <textarea
+          className={css.modalInput + " " + css.modalTextarea}
+          placeholder="Comment"
+        />
+        <Button text="Send" width="100%" height="52px" type="submit" />
+      </form>
     </Modal>
   );
 };
